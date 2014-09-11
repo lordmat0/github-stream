61c463371deca66542467ec4b74a81e8cb34833a
Matthew <lordmat@gmail.com>
2014-09-11 19:45:52 +0100

    Added UserList which is used to xml mapping in GitHubAPIRest

---
+++ src/main/java/com/lordmat/githubstream/bean/UserList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.lordmat.githubstream.bean;
+
+import java.util.List;
+
+/**
+ *
+ * @author mat
+ */
+public class UserList {
+    
+    private List<String> users;
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
+}
--- src/main/java/com/lordmat/githubstream/web/GitHubAPIRest.java
+++ src/main/java/com/lordmat/githubstream/web/GitHubAPIRest.java
@@ -3,10 +3,13 @@ package com.lordmat.githubstream.web;
 import com.lordmat.githubstream.api.GitHubAPI;
 import com.lordmat.githubstream.api.GitHubUser;
 import com.lordmat.githubstream.api.GitHubCommit;
+import com.lordmat.githubstream.bean.UserList;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
@@ -28,38 +31,52 @@ public class GitHubAPIRest {
 
     @Context
     private UriInfo context;
-    
-    private GitHubAPI gitHubApi;
+    private final GitHubAPI gitHubAPI;
 
     /**
      * Created every time the webAPI is hit
      */
     public GitHubAPIRest() {
         System.out.println("GitHubAPI created");
-        gitHubApi = new GitHubAPI();
+        gitHubAPI = new GitHubAPI();
     }
 
     /**
+     * Returns a details of users requested, example jQuery code below.
+     *
+     * <p>
+     *
+     * <code>
+     * $.ajax('res/githubapi/user/', {
+     * <br>
+     * contentType: 'application/json',
+     * <br>
+     * type: 'POST',
+     * <br>
+     * data: JSON.stringify({"users":["user1","user2"]})
+     * <br>
+     * });
+     * </code>
      *
      * @param users List of users to find information about
      * @return A list of github users details requested
      */
     @Path("user/")
     @POST
-    public List<GitHubUser> getUsers(List<String> users) {
-        return gitHubApi.findUser(users);
+    public List<GitHubUser> getUsers(UserList users) {
+        return gitHubAPI.findUser(users.getUsers());
     }
 
     /**
      *
-     * @param lasestCommitId The commit ID to check against
+     * @param latestDate The commit ID to check against
      * @return An empty list or commits that come after the lastestCommitId
      *
      */
     @Path("commit/new")
     @POST
-    public List<GitHubCommit> getNewCommits(String lastCommitDate) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<GitHubCommit> getNewCommits(String latestCommitDate) {
+        return gitHubAPI.checkForNewCommits(latestCommitDate);
     }
 
     /**
@@ -72,32 +89,5 @@ public class GitHubAPIRest {
     public List<GitHubCommit> getOldCommits(String earlistCommitId) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
-    @Path("test1/")
-    @POST
-    public GitHubUser test1(GitHubUser user) {
-        if (user != null) {
-            System.out.println(user);
-            return user;
-        }
-
-        return new GitHubUser("Nope", "Didn't work", " try again");
-    }
-
-    /*
-    this works but GET doesn't
-    
-     $.ajax('', {
-     contentType: 'application/json',
-     type: 'POST',
-     data: JSON.stringify({
-     'userName': 'No2pe',
-     'accountUrl': 'Didn\'t work',
-     'avatarUrl': ' try again'
-     }
-     )
-     });
-
-     */
 
 }
