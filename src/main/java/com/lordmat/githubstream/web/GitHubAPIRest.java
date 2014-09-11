06e7333a5eaf996b9ecc7a9981092bd1a62493f0
matthew <lordmat@gmail.com>
2014-09-11 14:31:44 +0100

    Added jackson json processing dependency Implemented web methods to GitHubApiRest

--- src/main/java/com/lordmat/githubstream/web/GitHubAPIRest.java
+++ src/main/java/com/lordmat/githubstream/web/GitHubAPIRest.java
@@ -1,5 +1,6 @@
 package com.lordmat.githubstream.web;
 
+import com.lordmat.githubstream.api.GitHubAPI;
 import com.lordmat.githubstream.api.GitHubUser;
 import com.lordmat.githubstream.api.GitHubCommit;
 import java.util.List;
@@ -7,8 +8,9 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -21,17 +23,20 @@ import javax.ws.rs.Produces;
 //TODO finish this class
 @Path("githubapi")
 @Produces("application/json")
-@Consumes("application/json")
+@Consumes(MediaType.APPLICATION_JSON)
 public class GitHubAPIRest {
 
     @Context
     private UriInfo context;
+    
+    private GitHubAPI gitHubApi;
 
     /**
      * Created every time the webAPI is hit
      */
     public GitHubAPIRest() {
         System.out.println("GitHubAPI created");
+        gitHubApi = new GitHubAPI();
     }
 
     /**
@@ -40,9 +45,9 @@ public class GitHubAPIRest {
      * @return A list of github users details requested
      */
     @Path("user/")
-    @GET
-    public List<GitHubUser> getUsers(String[] users) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    @POST
+    public List<GitHubUser> getUsers(List<String> users) {
+        return gitHubApi.findUser(users);
     }
 
     /**
@@ -52,8 +57,8 @@ public class GitHubAPIRest {
      *
      */
     @Path("commit/new")
-    @GET
-    public List<GitHubCommit> getNewCommits(String lasestCommitId) {
+    @POST
+    public List<GitHubCommit> getNewCommits(String lastCommitDate) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
@@ -63,9 +68,36 @@ public class GitHubAPIRest {
      * @return An empty list or commits that come before the earlistCommitId
      */
     @Path("commit/old")
-    @GET
+    @POST
     public List<GitHubCommit> getOldCommits(String earlistCommitId) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    @Path("test1/")
+    @POST
+    public GitHubUser test1(GitHubUser user) {
+        if (user != null) {
+            System.out.println(user);
+            return user;
+        }
+
+        return new GitHubUser("Nope", "Didn't work", " try again");
+    }
+
+    /*
+    this works but GET doesn't
+    
+     $.ajax('', {
+     contentType: 'application/json',
+     type: 'POST',
+     data: JSON.stringify({
+     'userName': 'No2pe',
+     'accountUrl': 'Didn\'t work',
+     'avatarUrl': ' try again'
+     }
+     )
+     });
+
+     */
 
 }
