6698b2fffa1073b6eb2be31b7361748726917de4
Matthew <lordmat@gmail.com>
2014-09-22 22:59:45 +0100

    Abstracted creating commits Added ajax post data handling for old commits (logic to call it is not yet implemented)

--- src/main/webapp/res/js/main.js
+++ src/main/webapp/res/js/main.js
@@ -15,32 +15,59 @@ $(function () {
     }, 5000);
 });
 
-
+/**
+ * Handles new commits by appending them to the top of the DOM
+ * 
+ * @param {type} data POST data returned from Ajax call
+ */
 function handleNewCommits(data) {
-    if (!data.length) {
-        // data is empty
-        return;
-    }
 
     for (var i = 0; i < data.length; i++) {
-        // Clone existing object
-        var $commit = $('.commit').first().clone(false);
-        
-        var user = data[i].userCommited;
-        var date =  data[i].date.substring(0, data[i].date.lastIndexOf('.')) + 'Z';
-      
-        
-        // Change values to new commit
-        $commit.find('.commit-id strong').text(data[i].id);
-        $commit.find('.commit-message div').text(data[i].message);
-        $commit.find('.commit-date strong').text(date);
-        $commit.find('.commit-accounturl')
-                .attr('href', user.accountUrl)
-                .find('strong').text(user.userName);
-        
-        $commit.find('.commit-user-avatar').attr('src', user.avatarUrl);
-        
+        var $commit = createCommit(data[i]);
+
         // Add the new commit to the DOM
         $('section').prepend($commit);
     }
+}
+
+/**
+ * 
+ * @param {type} data POST data returned from Ajax call
+ */
+function handleOldCommits(data) {
+    for (var i = 0; i < data.length; i++) {
+        var $commit = createCommit(data[i]);
+
+        // Add the new commit to the DOM
+        $('section').append($commit);
+    }
+}
+
+
+/**
+ * 
+ * @param {type} data Expected data = { userCommited, date, id, message, userCommited: { accountUrl, userName, avatarUrl }
+ * 
+ * @returns {createCommit.$commit|jQuery} A 
+ */
+function createCommit(data) {
+    // Clone existing object
+    var $commit = $('.commit').first().clone(false);
+
+    var user = data.userCommited;
+    var date = data.date.substring(0, data.date.lastIndexOf('.')) + 'Z';
+
+
+    // Change values to new commit
+    $commit.find('.commit-id strong').text(data.id);
+    $commit.find('.commit-message div').text(data.message);
+    $commit.find('.commit-date strong').text(date);
+    $commit.find('.commit-accounturl')
+            .attr('href', user.accountUrl)
+            .find('strong').text(user.userName);
+
+    $commit.find('.commit-user-avatar').attr('src', user.avatarUrl);
+
+
+    return $commit;
 }
