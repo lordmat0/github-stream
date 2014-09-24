1fe6499b30fc1ef727d4ee9fa0b237d5c8b66309
matthew <lordmat@gmail.com>
2014-09-24 22:04:40 +0100

    Fixed issue where new or old commits could be stuck fading in

--- src/main/webapp/res/js/main.js
+++ src/main/webapp/res/js/main.js
@@ -1,13 +1,13 @@
 $(function () {
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         var ajaxCall = false;
-        return function(){
-            if(ajaxCall){
+        return function () {
+            if (ajaxCall) {
                 return; // already Calling
             }
-            
-            
+
+
             var totalHeight = document.body.offsetHeight;
             var visibleHeight = document.documentElement.clientHeight;
 
@@ -27,40 +27,53 @@ $(function () {
                     data: JSON.stringify({
                         "data": date
                     }),
-                    success: function(data){handleOldCommits(data); ajaxCall = false;}
+                    success: function (data) {
+                        handleOldCommits(data);
+                        ajaxCall = false;
+                    }
                 });
             }
         };
     }());
 
-    /*
-    $(window).scroll(function () {
-        var totalHeight = document.body.offsetHeight;
-        var visibleHeight = document.documentElement.clientHeight;
 
-        var scrollTop = document.documentElement.scrollTop;
+    /**
+     * 
+     * @param {type} data Expected data = { userCommited, date, id, message, userCommited: { accountUrl, userName, avatarUrl }
+     * 
+     * @returns {createCommit.$commit|jQuery} A 
+     */
+    createCommit = (function (data) {
+        // master commit here, fixes bug with cloning a fading element
+        // clone a commit on the DOM
+        var $masterCommit = $('.commit').first().clone(false);
 
-        // Where the scroll bar is currently
-        var currentScroll = scrollTop ? scrollTop : document.body.scrollTop;
+        // Closure scope
+        return function (data) {
+            var $commit = $masterCommit.clone(false);
 
-        if (totalHeight <= visibleHeight + currentScroll) {
-            // At bottom of the page
-            
-            var date = $('.commit').last().find('.commit-date strong').text();
-            
-            $.ajax('rest/githubapi/commit/old', {
-                contentType: 'application/json',
-                type: 'POST',
-                data: JSON.stringify({
-                    "data": date
-                }),
-                success: handleOldCommits
-            });
-        }
+            var user = data.userCommited;
 
-    });
-    */
+            var date = data.date;
 
+            // Add Z to the ender if not appended on at the server (seems to be an issue with osx)
+            date = date + (date.indexOf("Z") > -1 ? '' : 'Z');
+
+            // Change values to new commit
+            $commit.find('.commit-id strong').text(data.id);
+            $commit.find('.commit-message div').text(data.message);
+            $commit.find('.commit-date strong').text(date);
+            $commit.find('.commit-accounturl')
+                    .attr('href', user.accountUrl)
+                    .find('strong').text(user.userName);
+
+            $commit.find('.commit-user-avatar').attr('src', user.avatarUrl);
+
+            $commit.hide();
+
+            return $commit;
+        };
+    }());
 
 
     // Check for new Commits
@@ -116,34 +129,3 @@ function handleOldCommits(data) {
 }
 
 
-/**
- * 
- * @param {type} data Expected data = { userCommited, date, id, message, userCommited: { accountUrl, userName, avatarUrl }
- * 
- * @returns {createCommit.$commit|jQuery} A 
- */
-function createCommit(data) {
-    // Clone existing object
-    var $commit = $('.commit').first().clone(false);
-
-    var user = data.userCommited;
-    
-    var date = data.date;
-    
-    // Add Z to the ender if not appended on at the server (seems to be an issue with osx)
-    date = date + (date.indexOf("Z") > -1 ? '' : 'Z');
-    
-    // Change values to new commit
-    $commit.find('.commit-id strong').text(data.id);
-    $commit.find('.commit-message div').text(data.message);
-    $commit.find('.commit-date strong').text(date);
-    $commit.find('.commit-accounturl')
-            .attr('href', user.accountUrl)
-            .find('strong').text(user.userName);
-
-    $commit.find('.commit-user-avatar').attr('src', user.avatarUrl);
-    
-    $commit.hide();
-
-    return $commit;
-}
